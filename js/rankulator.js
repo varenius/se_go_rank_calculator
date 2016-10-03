59a3d5e5ac3cbbb11b30a2ee2959396c851877cc
Eskil Varenius <eskil.varenius@gmail.com>
2016-10-03 18:57:09 +0200

    Set pro and 8d,9d ranks equal to 7d in point calculation.

--- js/rankulator.js
+++ js/rankulator.js
@@ -11,12 +11,33 @@
 					val: 30 - i
 				});
 			};
-
-			for (i = 0; i < 9; i++) {
+            // add relative points up to 7 dan
+			for (i = 0; i < 7; i++) {
 				ranks.push({
 					text: (i + 1) + ' dan',
 					shortName: (i + 1) + 'd',
 					val: i + 30
+				});
+			};
+            
+			// set 8d and 9d to same points as 7d
+		   ranks.push({ 
+			   text: 8 + ' dan', 
+			   shortName: 8 + 'd', 
+			   val: 36
+		   });
+		   ranks.push({ 
+			   text: 9 + ' dan', 
+			   shortName: 9 + 'd', 
+			   val: 36
+		   }); 
+			
+			// set all pro ranks equal to 7d in point value
+			for (i = 0; i < 9; i++) {
+				ranks.push({
+					text: (i + 1) + ' pro',
+					shortName: (i + 1) + 'p',
+					val: 36
 				});
 			};
 
